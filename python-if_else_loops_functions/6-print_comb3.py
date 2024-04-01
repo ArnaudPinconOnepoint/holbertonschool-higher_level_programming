4b01991ea6b29a15285a12cf6a44cff8f9ae7909
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-02 10:17:19 +1100

    fix 9

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i * 10 + j), end=", ")
+        if (i == 8 and j == 9):
+            print("{:02d}".format(i * 10 + j), end="")
+        else:
+            print("{:02d}".format(i * 10 + j), end=", ")
