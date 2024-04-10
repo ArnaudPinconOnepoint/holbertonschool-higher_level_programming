e3882202ad1c92e22ce1975ef89a7a55f73a6c06
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-10 17:23:08 +1000

    on essaye 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    no_arg = len(sys.argv)-1
+    res = 0
+    for i in range(no_arg):
+        res += int(sys.argv[i+1])
+    print("{}".format(res, end=""))
