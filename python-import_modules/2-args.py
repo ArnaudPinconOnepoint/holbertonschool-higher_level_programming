61f35b8f36946b1cbdf8c950a00b7913fc07adf4
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-10 17:04:52 +1000

    test 2 mais jycrois pas

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def list_arg(*argv):
+    no_arg = len(argv)
+    if no_arg == 0:
+        print("0 arguments.")
+    elif no_arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(no_arg))
+    for i in range(no_arg):
+        print("{}: {}".format(i+1, argv[i]))
+
+""" if __name__ == "__main__":
+    a = 10
+    b = 5 """
