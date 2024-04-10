3745d675c59a58cd4cf3d9683fd97195e9669401
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-10 17:15:41 +1000

    fix 22

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 import sys
 
 if __name__ == "__main__":
-    no_arg = len(sys.argv)
+    no_arg = len(sys.argv - 1)
     if no_arg == 0:
         print("0 arguments.")
     elif no_arg == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(no_arg))
-    for i in range(no_arg):
-        print("{}: {}".format(i+1, sys.argv[i]))
+    for i in range(no_arg - 1):
+        print("{}: {}".format(i+1, sys.argv[i+1]))
