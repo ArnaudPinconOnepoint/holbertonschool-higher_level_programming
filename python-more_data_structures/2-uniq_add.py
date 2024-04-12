2fc43ab2382cf0d3cf7f46158de1e4246b375f03
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-12 15:29:01 +1000

    add other solution for 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,3 +4,9 @@ def uniq_add(my_list=[]):
     for i in my_list:
         my_dict.setdefault(i, i)
     return sum(my_dict.values())
+
+
+# other solution
+#mylist = ["a", "b", "a", "c", "c"]
+#mylist = list(dict.fromkeys(mylist))
+#print(mylist)
