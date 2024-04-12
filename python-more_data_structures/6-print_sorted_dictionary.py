0e369b2a62d87aa1e1c5085a0133a468f8da69dc
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-12 16:07:15 +1000

    super fix 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     new_dic = dict(sorted(a_dictionary.items()))
-    list(map(lambda x: print(x, end='\n'), new_dic.items()))
+    for k, v in new_dic.items():
+        print('{}: {}'.format(k, v, end="\n"))
