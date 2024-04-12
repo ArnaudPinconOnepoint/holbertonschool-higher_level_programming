43c777085b3d41b703a2b2aaf41b30e2da6a6ce0
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-12 16:54:30 +1000

    fix 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
     new_dic = a_dictionary.copy()
     best_val = 0
     best_student = None
     for k, v in new_dic.items():
-        if v is None or new_dic is None:
-            return None
         if v > best_val:
             best_val = v
             best_student = k
