f1bee1d3ecd40b1f89c08837599498bf4e2703c4
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-12 14:44:51 +1000

    add 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = list(map(lambda x: switch(x, search, replace), my_list))
+    return new_list
+
+
+def switch(x, search, replace):
+    if x == search:
+        return replace
+    return x
