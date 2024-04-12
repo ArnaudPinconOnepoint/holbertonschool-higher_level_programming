4dfefcd7189c39ec1379f5ae00ca9627a12355b1
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-12 15:59:18 +1000

    fix 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print(dict(sorted(a_dictionary.items())))
+    new_dic = dict(sorted(a_dictionary.items()))
+    list(map(lambda x: print(x, end='\n'), new_dic.items()))
