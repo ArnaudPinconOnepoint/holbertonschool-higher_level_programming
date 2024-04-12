32f5e0204edd6d8a70f99fed8fcde51d83c5f0bf
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-12 16:51:49 +1000

    add 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    new_dic = a_dictionary.copy()
+    best_val = 0
+    best_student = None
+    for k, v in new_dic.items():
+        if v is None:
+            return None
+        if v > best_val:
+            best_val = v
+            best_student = k
+    return best_student
