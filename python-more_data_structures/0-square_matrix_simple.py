cbb631e1338efa360738ed78fa08cf115bd1d1a6
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-12 13:54:28 +1000

    fix 0 with list

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = map(square_value, matrix)
-    return new_matrix
+    return [list(row) for row in new_matrix]
         
 
 def square_value(x):
