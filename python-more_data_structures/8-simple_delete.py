261759560d8df4e38dd9efa402132d5562c801fc
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-12 16:25:54 +1000

    add 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    return a_dictionary.pop(key)
+    if (a_dictionary.get(key) != None):
+        a_dictionary.pop(key)
+    return a_dictionary
