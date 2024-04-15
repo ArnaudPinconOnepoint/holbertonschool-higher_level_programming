34f120021be30afa4af59374f06cc3fefd59fde1
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-15 16:27:06 +1000

    fix 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     nb = 0
     try:
         for i in range (0, x):
-            print({}.format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             nb +=1
         return nb
     except IndexError:
