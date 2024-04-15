60326c8a8e987d31fceb3bf7cfea57c455f74d57
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-15 16:50:32 +1000

    fix 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     nb = 0
     try:
         for i in range(0, x):
-            print("{:d}".format(my_list[i]), end="")
-            nb += 1
+            if (isinstance(my_list[i]), int):
+                print("{:d}".format(my_list[i]), end="")
+                nb += 1
         print()
         return nb
     except IndexError:
