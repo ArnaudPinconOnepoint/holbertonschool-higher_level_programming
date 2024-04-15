c4110b2b66c0ef5b9aed7f6ea3df773dea6d62ee
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-15 16:57:57 +1000

    add 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    res = 0
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        print("Inside result: {res}".format(), end="\n")
