32e73667d8527302910d82d04421ca95cb9569d8
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-15 16:23:56 +1000

    add 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    #number of digits returned
+    nb = 0
+    try:
+        for i in range (0, x):
+            print(my_list[i])
+            nb +=1
+        return nb
+    except IndexError:
+        return nb
