026dc7a2574eab5d07dfcf7b72258d510b534c6a
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-15 16:43:44 +1000

    add 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nb = 0
+    try:
+        for i in range(0, x):
+            print("{:d}".format(my_list[i]), end="")
+            nb += 1
+        print()
+        return nb
+    except IndexError:
+        print()
+        return nb
+    except ValueError:
+        print()
+        return nb
+    except TypeError:
+        print()
+        return nb
