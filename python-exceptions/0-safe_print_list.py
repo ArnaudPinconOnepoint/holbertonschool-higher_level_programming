90451d4587d2788ee4cfbe769f66422fa7b25722
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-15 16:32:48 +1000

    fix 0 pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    #number of digits returned
     nb = 0
     try:
-        for i in range (0, x):
+        for i in range(0, x):
             print("{}".format(my_list[i]), end="")
-            nb +=1
+            nb+=1
         print()
         return nb
     except IndexError:
