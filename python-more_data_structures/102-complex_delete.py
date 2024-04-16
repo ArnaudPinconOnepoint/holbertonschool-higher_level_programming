fc94d37c0ef832a4c7075f734d00cbed5ac37aae
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-16 17:33:39 +1000

    fix 102

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,4 +4,5 @@ def complex_delete(a_dictionary, value):
     for i in a_dictionary.keys():
         if a_dictionary.get(i) == value:
             new_dic.pop(i)
+    a_dictionary = new_dic
     return new_dic
