2c60c28f83a8ee4eb4270b063c0bd9b9c981ff98
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-16 15:39:18 +1000

    fix 101

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -4,8 +4,17 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        res = fct(args)
+        res = fct(*args)
     except TypeError as e:
+        print('Exception: {}'.format(e), end="\n", file=sys.stderr)
+        res = None
+    except ValueError as e:
+        print('Exception: {}'.format(e), end="\n", file=sys.stderr)
+        res = None
+    except ZeroDivisionError as e:
+        print('Exception: {}'.format(e), end="\n", file=sys.stderr)
+        res = None
+    except NameError as e:
         print('Exception: {}'.format(e), end="\n", file=sys.stderr)
         res = None
     finally:
