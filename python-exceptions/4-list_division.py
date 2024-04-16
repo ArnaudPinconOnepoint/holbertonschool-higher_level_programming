a14f174027e28f29c8ea17232b1cbe58441cc47a
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-16 13:23:11 +1000

    fix 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length=[]):
     res = 0
     try:
         for i in range(len(my_list_1)):    
