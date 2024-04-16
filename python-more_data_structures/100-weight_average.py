0227516479fef0959efa5ef23f79cff92739d65f
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-16 16:48:09 +1000

    fix 100

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
     bottom = 0
     if len(my_list) == 0:
         return 0
-    for i in range (my_list):
+    for i in my_list:
         top=top+i.get(0)*i.get(1)
         bottom=bottom+i.get(1)
     return top/bottom
