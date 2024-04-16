ff6fbf90937ead5ce15c555d252513be2ab58847
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-16 15:17:34 +1000

    add 100

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value), end='\n')
+        return True
+    except TypeError as e:
+        print("Exception:{}".format(e), end="")
+        return False
+        
+
+    
