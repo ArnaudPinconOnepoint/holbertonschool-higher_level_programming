b4cd20eee7554a900593a8c9ec6ec1a8d37da19f
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-18 09:59:00 +1000

    add 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+"""This module contains class Square"""
+
+
+class Square:
+    """This class is Square"""
+    __size = 10
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter size"""
+        try:
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        except (TypeError, ValueError) as e:
+            raise e
+
+    def area(self):
+        """Return area"""
+        area = self.__size*self.__size
+        return area
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(1, self.area()+1):
+                if(i % self.__size):
+                    print("#", end="")
+                else:
+                    print("#", end="\n")
