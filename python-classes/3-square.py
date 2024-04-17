1bda88fbb36ccc225940e50a5d33115606c35106
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-18 09:35:34 +1000

    add 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""This module contains class Square"""
+
+
+class Square:
+    """This class is Square"""
+    __size = 10
+
+    def __init__(self, size=0):
+        """Initial instance"""
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except (TypeError, ValueError) as e:
+            raise e
+    
+    
+    def area(self):
+        """Return instance"""
+        return self
