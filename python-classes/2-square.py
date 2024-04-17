892cee36555c4e7c2419768bc083bc85b07b5cdf
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-18 09:30:01 +1000

    fix 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,10 +9,10 @@ class Square:
     def __init__(self, size=0):
         """Initial instance"""
         try:
-            if size < 0:
-                raise ValueError("size must be >= 0")
             if not isinstance(size, int):
                 raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
             self.__size = size
         except (TypeError, ValueError) as e:
             raise e
