f5804be3fc749e7af248b2fab7e23778c59071cc
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-17 13:51:38 +1000

    fix 102

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0  # Initialisation de la variable result
-
-    # Boucle for
     for i in range(1, 3):  # Range de 1 à 2
         try:
             if i > a:  # Vérifie si i est plus grand que a
-                raise Exception('Too far')  # Lance une exception si i est trop grand
-        except Exception:
-            result += (a ** b) / i  # Calcule (a**b)/i et ajoute le résultat à result
-        else:
-            result += a + b  # Si aucune exception n'est levée, ajoute a+b à result
-        finally:
-            pass  # Ne fait rien dans le bloc finally
-
+                raise Exception('Too far')
+            else:
+                result = (a ** b) / i
+        except:
+            result = a + b
+            break
     return result  # Retourne la valeur de result à la fin de la boucle et du code
