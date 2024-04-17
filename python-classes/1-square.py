4d4165a0482a82f95ec381ce84e81b866ec3eab7
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-17 16:15:06 +1000

    add 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""This module contains class Square"""
+
+
+class Square:
+    """This class is Square"""
+    __size = 10 
+
+    def __init__(self, new_size=3):
+        """Initial instance"""
+        self.__size = new_size
