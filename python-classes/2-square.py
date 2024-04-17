e0b8e38c4408f968d422163eb3eb9ae72dbc9b17
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-17 16:22:25 +1000

    add 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""This module contains class Square"""
+
+
+class Square:
+    """This class is Square"""
+    __size = 10
+
+    def __init__(self, size=0):
+        """Initial instance"""
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            self.__size = size
+        except (TypeError, ValueError) as e:
+            raise e
