0a64176b405b83dd83867bb6f3a4c495f417ff06
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-18 10:35:19 +1000

    add 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,61 @@
 #!/usr/bin/python3
+"""This module contains class Square"""
+
+
+class Square:
+    """This class is Square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Getter size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter size"""
+        try:
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        except (TypeError, ValueError) as e:
+            raise e
+    
+    @property
+    def position(self):
+        """Getter position"""
+        return self.__position
+
+    @size.setter
+    def position(self, value):
+        """Setter position"""
+        try:
+            if not isinstance(value, (int, int)):
+                raise TypeError("position must be a tuple of 2 positive integers")
+            self.__position = value
+        except (TypeError) as e:
+            raise e
+
+    def area(self):
+        """Return area"""
+        area = self.__size*self.__size
+        return area
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            for j in range (0, self.__position[1]):
+                print()
+            print(' '*self.__position[0], end="")
+            for i in range(1, self.area()+1):
+                if (i % self.__size):
+                    print("#", end="")
+                else:
+                    print("#", end="\n")
+                    print(' '*self.__position[0], end="")
