82d18c3c168227d1bc14957e983db47c71e6378d
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-19 12:46:17 +1000

    add 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -4,4 +4,41 @@
 
 class Rectangle:
     """This class is Rectangle"""
-    pass
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width*self.height
+    
+    def perimeter(self):
+        return self.width * 2 + self.height * 2
