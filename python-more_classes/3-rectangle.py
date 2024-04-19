6a381575757e1224a6df74de8327d2072e8d417c
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-19 13:17:45 +1000

    fix

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
 
     def __repr__(self):
         return f"<3-rectangle.Rectangle object"
-    
+
     def __str__(self):
         if self.height == 0 or self.width == 0:
             return ""
