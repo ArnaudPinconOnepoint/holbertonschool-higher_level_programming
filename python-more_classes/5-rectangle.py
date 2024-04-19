1bc5003b3011f6bff95758b01e47ff1db4c3b29d
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-19 13:42:13 +1000

    add 6 + fix 5

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -4,12 +4,10 @@
 
 class Rectangle:
     """This class is Rectangle"""
-    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        self.number_of_instances += 1
 
     @property
     def width(self):
@@ -62,5 +60,4 @@ class Rectangle:
             return rectangle_str
 
     def __del__(self):
-        self.number_of_instances -= 1
         print("Bye rectangle...")
