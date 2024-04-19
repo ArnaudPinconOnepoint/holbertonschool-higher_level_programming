a7e5f8661f5ba5194a0a3c5b4eb1a5f686da39a6
Arnaud Pinçon <gop_arnaud@m1.com.sg>
2024-04-19 13:10:05 +1000

    test

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
         return self.width * 2 + self.height * 2
 
     def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})"
+        return Rectangle(self)
     
     def __str__(self):
         if self.height == 0 or self.width == 0:
