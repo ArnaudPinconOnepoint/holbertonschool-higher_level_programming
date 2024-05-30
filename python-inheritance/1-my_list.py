3adaf639a0b3975e36239cfb929732147d3cb76d
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-05-30 15:03:36 +1000

    add 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module's is documented
+"""
+
+
+class MyList(list):
+    """
+    This class is documented
+    """
+    def print_sorted(self):
+        """
+        This fc is documented
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
