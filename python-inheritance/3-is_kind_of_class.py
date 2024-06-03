31403e7bbd30d4f7f39959b90ace4aa6765a3d2a
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-03 15:09:50 +1000

    add 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module's is documented
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+
+    Parameters:
+    - obj: The object to check.
+    - a_class: The class to compare against.
+
+    Returns:
+    - True if obj is an instance of a_class; False otherwise.
+    """
+    return isinstance(obj, a_class)
