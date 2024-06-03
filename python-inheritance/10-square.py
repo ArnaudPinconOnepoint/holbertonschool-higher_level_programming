e7fdf128b1aaf2ce325f7be484ce0e04ed1344f1
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-03 16:21:14 +1000

    add 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module contains class BaseGeometry.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle."""
+    def __init__(self, size):
+        """
+        Initialize the Square with size.
+        Parameters:
+        - size: The size of the square.
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is not positive.
+        """
+        self.__size = 0
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)  # Call the Rectangle constructor with size as width and height
+
+
+
+    def area(self):
+        """
+        Calculate the area of the square.
+        Returns:
+        - The area of the square (size * size).
+        """
+        return self.__size * self.__size
