1d3a7513961616d7fb6ce21448fb65d3ebb46f4d
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-03 15:56:10 +1000

    fix pep

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
         Raises an Exception since it is not implemented in the base class.
         """
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """
         Validate that a parameter is a positive integer.
