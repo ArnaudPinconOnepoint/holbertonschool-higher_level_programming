e96e6fec549ac1fbe24baf7c4181d272237b62f3
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 13:16:08 +1000

    true fix

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,6 +8,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ description """
-    representation = json.dumps(my_obj)
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(representation)
+        json.dump(my_obj, f)
