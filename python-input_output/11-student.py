7ab9e7923130f3dcd78f3a1f9a35995445463396
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 15:52:34 +1000

    fixx 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -16,10 +16,15 @@ class Student:
         """Retrieves a filtered or full dictionary representation of a Student instance."""
         if attrs is None:
             # If attrs is None, retrieve all attributes
-            return self.__dict__
+            return self.__dict__  
         # Filter the attributes based on attrs list
         json_dict = {}
         for attr in attrs:
             if hasattr(self, attr):
                 json_dict[attr] = getattr(self, attr)
         return json_dict
+
+    def reload_from_json(self, json_data):
+        """Replaces all attributes of the Student instance with values from a JSON dictionary."""
+        for attr, value in json_data.items():
+            setattr(self, attr, value)
