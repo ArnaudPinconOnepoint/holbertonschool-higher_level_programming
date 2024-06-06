33454b2aa218d86a8c773b6b71f1794a91818d1c
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 12:43:16 +1000

    return

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,10 +8,4 @@ def write_file(filename="", text=""):
     """ Write File """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
-    read_file(filename)
-
-def read_file(filename=""):
-    """ Read File """
-    with open(filename, encoding="utf-8") as f:
-        read_data = f.read()
-        print(f"{read_data}", end='')
+    return f
