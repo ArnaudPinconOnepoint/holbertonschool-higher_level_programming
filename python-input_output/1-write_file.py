732ac9c9058ec1fec8236b023f70e1548f0e5850
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 12:47:42 +1000

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,6 +7,6 @@ This module contains class BaseGeometry.
 def write_file(filename="", text=""):
     """ Write File """
     with open(filename, "w", encoding="utf-8") as f:
-        content = f.write(text)
-        size = len(content)
+        f.write(text)
+        size = len(f.read())
     return size
