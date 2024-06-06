e89804122c871a52f55f2132694449b5d10702b5
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 12:55:05 +1000

    fix 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,7 +8,4 @@ def append_write(filename="", text=""):
     """ Write File """
     with open(filename, 'a', encoding='utf-8') as f:
         f.write(text)
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        size = len(content)
-    return size
+    return len(text)
