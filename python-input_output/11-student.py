8e1f7f7db96cee90d01670cc2889e604ead89619
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 15:51:23 +1000

    add 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script adds all arguments to a Python list,
+and then saves them to a file.
+"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a filtered or full dictionary representation of a Student instance."""
+        if attrs is None:
+            # If attrs is None, retrieve all attributes
+            return self.__dict__
+        # Filter the attributes based on attrs list
+        json_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_dict[attr] = getattr(self, attr)
+        return json_dict
