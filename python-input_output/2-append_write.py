65344117791f48f23146baf65d86937550928e4c
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 12:53:18 +1000

    append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains class BaseGeometry.
+"""
+
+
+def append_write(filename="", text=""):
+    """ Write File """
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(text)
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
