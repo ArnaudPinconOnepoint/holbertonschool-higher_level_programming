632ae5c32e86eca14ced591960e4523b63545b8b
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 13:14:22 +1000

    fix add execption

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,11 +10,11 @@ def save_to_json_file(my_obj, filename):
     """ description """
     try:
         # Convert the object to a JSON string
-        representation = json.dumps(my_obj, default=list)
-        
+        representation = json.dumps(my_obj, default=list)        
         # Write the JSON string to a file
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(representation)
-    
+    except TypeError as e:
+        print(f"[TypeError] {e}")
     except PermissionError as e:
         print(f"[PermissionError] {e}")
