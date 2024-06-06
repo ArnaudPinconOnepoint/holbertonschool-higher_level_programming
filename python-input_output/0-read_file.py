873b93ab704bb971fffa8ffaffede285981d5293
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-06-06 11:57:46 +1000

    add 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains class BaseGeometry.
+"""
+
+
+def read_file(filename=""):
+    """ Read File """
+    with open('workfile', encoding="utf-8") as f:
+        f.read()
