5352d67abc3497398bbb680ce38198f67cad1d82
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-07-01 17:25:42 +1000

    add tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,35 +12,115 @@ class TestRectangle(unittest.TestCase):
     Classe de tests pour la classe Rectangle.
     """
 
-    def test_area(self):
+    def test_constructor_exists(self):
         """
-        Teste la méthode area.
+        Vérifie l'existence du constructeur de Rectangle avec différentes combinaisons d'arguments.
         """
-        pass  # Ajoutez vos tests ici
+        Rectangle(1, 2)
+        Rectangle(1, 2, 3)
+        Rectangle(1, 2, 3, 4)
+        Rectangle("1", 2)
+        Rectangle(1, "2")
+        Rectangle(1, 2, "3")
+        Rectangle(1, 2, 3, "4")
+        Rectangle(1, 2, 3, 4, 5)
+        Rectangle(-1, 2)
+        Rectangle(1, -2)
+        Rectangle(0, 2)
+        Rectangle(1, 0)
+        Rectangle(1, 2, -3)
+        Rectangle(1, 2, 3, -4)
 
-    def test_display(self):
+    def test_area_exists(self):
         """
-        Teste la méthode display.
+        Vérifie l'existence de la méthode area().
         """
-        pass  # Ajoutez vos tests ici
+        rect = Rectangle(2, 3)
+        self.assertEqual(rect.area(), 6)
 
-    def test_str(self):
+    def test_str_exists(self):
         """
-        Teste la méthode __str__.
+        Vérifie l'existence de la méthode __str__() pour Rectangle.
         """
-        pass  # Ajoutez vos tests ici
+        rect = Rectangle(2, 3, 1, 1, 1)
+        self.assertEqual(str(rect), "[Rectangle] (1) 1/1 - 2/3")
 
-    def test_update(self):
+    def test_display_exists(self):
         """
-        Teste la méthode update.
+        Vérifie l'existence de la méthode display() pour Rectangle.
         """
-        pass  # Ajoutez vos tests ici
+        rect = Rectangle(2, 3)
+        self.assertIsNone(rect.display())
 
-    def test_to_dictionary(self):
+    def test_to_dictionary_exists(self):
         """
-        Teste la méthode to_dictionary.
+        Vérifie l'existence de la méthode to_dictionary() pour Rectangle.
         """
-        pass  # Ajoutez vos tests ici
+        rect = Rectangle(2, 3, 1, 1, 1)
+        self.assertEqual(rect.to_dictionary(), {'id': 1, 'width': 2, 'height': 3, 'x': 1, 'y': 1})
+
+    def test_update_exists(self):
+        """
+        Vérifie l'existence de la méthode update() pour Rectangle.
+        """
+        rect = Rectangle(2, 3, 1, 1, 1)
+        rect.update(89)
+        self.assertEqual(rect.id, 89)
+        rect.update(89, 1)
+        self.assertEqual(rect.width, 1)
+        rect.update(89, 1, 2)
+        self.assertEqual(rect.height, 2)
+        rect.update(89, 1, 2, 3)
+        self.assertEqual(rect.x, 3)
+        rect.update(89, 1, 2, 3, 4)
+        self.assertEqual(rect.y, 4)
+        rect.update(id=89)
+        self.assertEqual(rect.id, 89)
+        rect.update(id=89, width=1)
+        self.assertEqual(rect.width, 1)
+        rect.update(id=89, width=1, height=2)
+        self.assertEqual(rect.height, 2)
+        rect.update(id=89, width=1, height=2, x=3)
+        self.assertEqual(rect.x, 3)
+        rect.update(id=89, width=1, height=2, x=3, y=4)
+        self.assertEqual(rect.y, 4)
+
+    def test_create_exists(self):
+        """
+        Vérifie l'existence de la méthode create() pour Rectangle.
+        """
+        rect = Rectangle.create(**{'id': 89})
+        self.assertEqual(rect.id, 89)
+        rect = Rectangle.create(**{'id': 89, 'width': 1})
+        self.assertEqual(rect.width, 1)
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual(rect.height, 2)
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual(rect.x, 3)
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual(rect.y, 4)
+
+    def test_save_to_file_exists(self):
+        """
+        Vérifie l'existence de la méthode save_to_file() pour Rectangle.
+        """
+        Rectangle.save_to_file(None)
+        Rectangle.save_to_file([])
+        Rectangle.save_to_file([Rectangle(1, 2)])
+
+    def test_load_from_file_exists(self):
+        """
+        Vérifie l'existence de la méthode load_from_file() pour Rectangle.
+        """
+        with self.assertRaises(FileNotFoundError):
+            Rectangle.load_from_file()
+
+    def test_load_from_file_when_file_exists(self):
+        """
+        Vérifie l'existence de la méthode load_from_file() pour Rectangle quand le fichier existe.
+        """
+        # Vous pouvez remplacer le chemin par celui de votre fichier de test existant
+        pass
 
 
 if __name__ == '__main__':
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -12,35 +12,92 @@ class TestSquare(unittest.TestCase):
     Classe de tests pour la classe Square.
     """
 
-    def test_area(self):
+    def test_constructor_exists(self):
         """
-        Teste la méthode area.
+        Vérifie l'existence du constructeur de Square avec différentes combinaisons d'arguments.
         """
-        pass  # Ajoutez vos tests ici
+        Square(1)
+        Square(1, 2)
+        Square(1, 2, 3)
+        Square("1")
+        Square(1, "2")
+        Square(1, 2, "3")
+        Square(1, 2, 3, 4)
+        Square(-1)
+        Square(1, -2)
+        Square(1, 2, -3)
+        Square(0)
 
-    def test_display(self):
+    def test_str_exists(self):
         """
-        Teste la méthode display.
+        Vérifie l'existence de la méthode __str__() pour Square.
         """
-        pass  # Ajoutez vos tests ici
+        square = Square(2, 3, 1, 1)
+        self.assertEqual(str(square), "[Square] (1) 3/1 - 2")
 
-    def test_str(self):
+    def test_to_dictionary_exists(self):
         """
-        Teste la méthode __str__.
+        Vérifie l'existence de la méthode to_dictionary() pour Square.
         """
-        pass  # Ajoutez vos tests ici
+        square = Square(2, 3, 1, 1)
+        self.assertEqual(square.to_dictionary(), {'id': 1, 'size': 2, 'x': 3, 'y': 1})
 
-    def test_update(self):
+    def test_update_exists(self):
         """
-        Teste la méthode update.
+        Vérifie l'existence de la méthode update() pour Square.
         """
-        pass  # Ajoutez vos tests ici
+        square = Square(2, 3, 1, 1)
+        square.update(89)
+        self.assertEqual(square.id, 89)
+        square.update(89, 1)
+        self.assertEqual(square.size, 1)
+        square.update(89, 1, 2)
+        self.assertEqual(square.x, 2)
+        square.update(89, 1, 2, 3)
+        self.assertEqual(square.y, 3)
+        square.update(id=89)
+        self.assertEqual(square.id, 89)
+        square.update(id=89, size=1)
+        self.assertEqual(square.size, 1)
+        square.update(id=89, size=1, x=2)
+        self.assertEqual(square.x, 2)
+        square.update(id=89, size=1, x=2, y=3)
+        self.assertEqual(square.y, 3)
 
-    def test_to_dictionary(self):
+    def test_create_exists(self):
         """
-        Teste la méthode to_dictionary.
+        Vérifie l'existence de la méthode create() pour Square.
         """
-        pass  # Ajoutez vos tests ici
+        square = Square.create(**{'id': 89})
+        self.assertEqual(square.id, 89)
+        square = Square.create(**{'id': 89, 'size': 1})
+        self.assertEqual(square.size, 1)
+        square = Square.create(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual(square.x, 2)
+        square = Square.create(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual(square.y, 3)
+
+    def test_save_to_file_exists(self):
+        """
+        Vérifie l'existence de la méthode save_to_file() pour Square.
+        """
+        Square.save_to_file(None)
+        Square.save_to_file([])
+        Square.save_to_file([Square(1)])
+
+    def test_load_from_file_exists(self):
+        """
+        Vérifie l'existence de la méthode load_from_file() pour Square.
+        """
+        with self.assertRaises(FileNotFoundError):
+            Square.load_from_file()
+
+    def test_load_from_file_when_file_exists(self):
+        """
+        Vérifie l'existence de la méthode load_from_file() pour Square quand le fichier existe.
+        """
+        # Vous pouvez remplacer le chemin par celui de votre fichier de test existant
+        pass
 
 
 if __name__ == '__main__':
