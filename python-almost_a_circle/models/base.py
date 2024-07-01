1bd3f417dccee478043c3e6134b880d005dde89a
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-07-01 16:42:52 +1000

    fix pep8

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -99,9 +99,9 @@ class Base:
             instance: Instance of the class with
             attributes set from dictionary.
         """
-        if 'size' in dictionary: # the object is a square
+        if 'size' in dictionary:
             dummy_instance = cls(1)
-        else: # the object is a rectangle
+        else:
             dummy_instance = cls(1, 1)
 
         dummy_instance.update(**dictionary)
@@ -116,13 +116,15 @@ class Base:
             list: List of instances loaded from the JSON file.
                 If the file doesn't exist, returns an empty list.
         """
-        filename = f"{cls.__name__}.json"  # Construct filename based on class name
+        filename = f"{cls.__name__}.json"
         try:
             with open(filename, 'r') as f:
                 json_string = f.read()  # Read JSON string from file
         except FileNotFoundError:
             return []  # Return empty list if file doesn't exist
 
-        list_dicts = cls.from_json_string(json_string)  # Convert JSON string to list of dictionaries
-        instances = [cls.create(**dict_obj) for dict_obj in list_dicts]  # Create instances from dictionaries
+        # Convert JSON string to list of dictionaries
+        list_dicts = cls.from_json_string(json_string)
+        # Create instances from dictionaries
+        instances = [cls.create(**dict_obj) for dict_obj in list_dicts]
         return instances
