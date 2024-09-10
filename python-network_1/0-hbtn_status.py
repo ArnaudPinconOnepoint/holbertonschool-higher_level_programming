e7abe9022e1dcec8c0350f662699266bba1f656e
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-10 14:33:53 +1000

    add network

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+0-hbtn_status
+
+This module check the status of a website
+
+"""
+
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+   html = response.read()
+   print("Body response:")
+   print("-", html)
