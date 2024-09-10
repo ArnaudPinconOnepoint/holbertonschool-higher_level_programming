b3e215cb6a86cb7f8039615d023b0234ef1d29bf
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-10 15:00:46 +1000

    add 1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+1-hbtn_header
+
+This module takes a URL as a command-line argument, sends a request to the URL,
+and displays the value of the X-Request-Id variable found in the header of the response.
+"""
+
+import urllib.request
+import sys
+
+def fetch_request_id(url):
+    """Fetches and displays the value of the X-Request-Id header from a given URL."""
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        headers = response.info()
+        request_id = headers.get('X-Request-Id')
+        if request_id:
+            print(request_id)
+        else:
+            print('No X-Request-Id header found')
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
+    else:
+        fetch_request_id(sys.argv[1])
