e400720541d85fb5134a8acbf8a1c4641b82c721
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-10 14:40:26 +1000

    fix

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,10 +2,12 @@
 """
 0-hbtn_status
 
-This module check the status of a website
-
+This module checks the status of a website.
 """
 
 import urllib.request
+
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-   html = response.read()
+    html = response.read()
+
+print(html.decode('utf-8'))
