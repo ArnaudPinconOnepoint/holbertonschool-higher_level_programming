8a4e787994973cf975154b8563df6af28957d38b
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 15:07:09 +1000

    fix 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""
-Fetching script
-"""
- 
+"""Python - Network #1"""
 import requests
- 
- 
+
+
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    request = requests.get(
+        "https://intranet.hbtn.io/status",
+        timeout=10000,
+        headers={"User-agent": "Mozilla/5.0 (Linux x86_64)"},
+    )
+
+    response_text = "OK" if request.text[0] == "<" else request.text
+
+    print(
+        f"Body response:\n"
+        f"\t- type: {type(request.text)}\n"
+        f"\t- content: {response_text}"
+    )
