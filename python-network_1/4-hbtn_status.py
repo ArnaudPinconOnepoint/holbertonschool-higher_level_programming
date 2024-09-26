13cc9995d4b3de68d029aed1df6ff1645ef25e68
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 14:50:29 +1000

    max code

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
 """
-
-This module fetches and displays the status of a website test.
+Fetching script
 """
-
+ 
 import requests
-
-
-def fetch_status(url):
-    """Fetches and displays the status of a website."""
-    try:
-        r = requests.get(url)
-    except requests.exceptions as e:
-        print(f"Error code: {e.code}")
-        return
-
-    print("Body response:")
-    print(f"    - type: {type(r.text)}")
-    print(f"    - content: {r.text}")
-
-
+ 
+ 
 if __name__ == "__main__":
-    fetch_status("https://intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
