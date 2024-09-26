d5ac55e0f28b642c8231cac27ea233e406c635af
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 10:52:28 +1000

    fix

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """
 script that takes in a URL and an email
 sends a POST request to the passed URL with the email as a parameter
-and displays the body of the response
+and displays the body of the response toto
 """
 import sys
 import urllib.parse
