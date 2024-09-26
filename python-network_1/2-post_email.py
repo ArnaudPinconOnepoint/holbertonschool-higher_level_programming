9d5a3e0e4a3b1f4d0633bb4aa3d867cc631d12be
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 14:21:09 +1000

    fix

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,13 +7,13 @@ and displays the body of the response toto
 import sys
 import urllib.parse
 import urllib.request
- 
- 
+
+
 if __name__ == "__main__":
     url = sys.argv[1]
     body = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(body).encode("ascii")
- 
+
     request = urllib.request.Request(url, data=data, method="POST")
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
