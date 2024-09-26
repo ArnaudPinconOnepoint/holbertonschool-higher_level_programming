f52ee0e0a41d1ad220b8f2989fef3b69a4134853
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 15:11:43 +1000

    fix 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,17 +7,21 @@ This module fetches and displays the status of a website.
 
 import urllib.request
 
+
 def fetch_status():
     """Fetches and displays the status of a website."""
-    url='https://intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'
     user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-    headers = {'User-Agent': user_agent}
-    with urllib.request.urlopen(url) as response:
-       body = response.read()
-       print("Body response:")
-       print(f"    - type: {type(body)}")
-       print(f"    - content: {body}")
-       print(f"    - utf8 content: {body.decode('utf-8')}")
+    headers = {"User-agent": user_agent}
+
+    request = urllib.request.Request(url, headers=headers)
+
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        print(f"    - utf8 content: {body.decode('utf-8')}")
 
 if __name__ == "__main__":
     fetch_status()
