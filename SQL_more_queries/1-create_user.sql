591a8ea4cc72642ce317a75ebdb0f9bd7a60329d
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 15:49:28 +1000

    fake fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,12 @@
+-- Create the user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+
+/* 
 -- creates the MySQL server user
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost; 
+*/
