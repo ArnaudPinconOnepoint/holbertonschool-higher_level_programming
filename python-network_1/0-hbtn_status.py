84b137b434912c46a1f0333181c856e30992120f
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 15:34:12 +1000

    fix pycode

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,8 @@
 """
 0-hbtn_status
 
-This script fetches the status of a website using urllib with a User-Agent header.
+This script fetches the status of a website
+using urllib with a User-Agent header.
 """
 
 import urllib.request
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     headers = {"User-Agent": "Mozilla/5.0 (Linux x86_64)"}
     request = urllib.request.Request(url)
-    with urllib.request.urlopen(request, timeout=10000) as response:
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print(f"\t- type: {type(body)}")
