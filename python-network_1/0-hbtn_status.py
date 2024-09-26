bea362a03630c20c09ad6252d007c37c9e47276c
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 15:30:14 +1000

    fix 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -10,15 +10,9 @@ import urllib.request
 
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
-    
-    # User-Agent header
     headers = {"User-Agent": "Mozilla/5.0 (Linux x86_64)"}
-
-    # Create a request object with the headers
-    request = urllib.request.Request(url, headers=headers)
-
-    # Use a with statement to handle the request and response
-    with urllib.request.urlopen(request) as response:
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request, timeout=10000) as response:
         body = response.read()
         print("Body response:")
         print(f"\t- type: {type(body)}")
