013b22ee48bbf353ed9fddcec5007723d68bcbb6
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-26 16:18:19 +1000

    fix 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     # headers = {"User-Agent": "Mozilla/5.0 (Linux x86_64)"}
     # request = urllib.request.Request(url, headers)
-    with urllib.request.urlopen(urllib.request.Request(url)) as response:
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print(f"\t- type: {type(body)}")
