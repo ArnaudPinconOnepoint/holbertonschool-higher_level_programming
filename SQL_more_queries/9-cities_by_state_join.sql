dc689d8f78b69873794d8a41a6e321501312e7c0
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-27 14:11:29 +1000

    fix 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,4 @@
--- Select all cities and their corresponding state names
-SELECT cities.id, 
-       cities.name, 
-       (SELECT states.name 
-        FROM states 
-        WHERE states.id = cities.state_id) AS state_name
-FROM cities 
-ORDER BY cities.id ASC;  -- Sort the results by cities.id in ascending order
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
