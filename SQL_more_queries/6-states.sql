5dcad1c3de0f1f495a501b8bcbb6290099a28d09
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-27 13:02:06 +1000

    fix 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
 -- Creates the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use the newly created database
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
