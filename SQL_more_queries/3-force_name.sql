c20d855c93d14d729533046e949a9d4602a4e930
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-09-27 12:53:09 +1000

    fix 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- Creates the table force_name if it does not exist
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
