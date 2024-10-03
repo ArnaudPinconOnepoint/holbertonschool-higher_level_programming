9bbc1cff288f1955badb16572a341b68ce03cbf9
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-10-03 15:04:50 +1000

    add 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module to list all states where name start with N from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def list_states(user, pwd, db):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost",
+                         user=user, passwd=pwd, db=db, port=3306)
+    cursor = db.cursor()
+
+    # Execute the query to select all states, ordered by states.id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC WHERE name LIKE 'N%'")
+
+    # Fetch all the results
+    states = cursor.fetchall()
+
+    # Display the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments are provided
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    # Get the arguments
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+    # List states
+    list_states(user, pwd, db)
