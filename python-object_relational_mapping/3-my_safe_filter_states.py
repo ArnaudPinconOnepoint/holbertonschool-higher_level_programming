3db806e3f87451ea2acb65ae65373d80712c90d2
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-10-03 15:36:04 +1000

    add 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module with param"""
+import MySQLdb
+import sys
+
+
+def list_states(user, pwd, db, search):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost",
+                         user=user, passwd=pwd, db=db, port=3306)
+    cursor = db.cursor()
+
+    # Execute the query    
+    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
+    cursor.execute(query, (search,))
+
+    cursor.execute(query)
+
+    # Fetch all the results
+    states = cursor.fetchall()
+
+    # Display the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+
+    # Get the arguments
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    search = sys.argv[4]
+
+    # List states
+    list_states(user, pwd, db, search)
