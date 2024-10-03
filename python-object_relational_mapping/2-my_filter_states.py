0c6679bb64c6f4ed275bd57d771f487193936d02
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-10-03 15:16:44 +1000

    add 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Module"""
+import MySQLdb
+import sys
+
+
+def list_states(user, pwd, db, search):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost",
+                         user=user, passwd=pwd, db=db, port=3306)
+    cursor = db.cursor()
+
+    # Execute the query
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (search,))
+
+    # Fetch all the results
+    states = cursor.fetchall()
+
+    # Display the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments are provided
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    # Get the arguments
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    search = sys.argv[4]
+
+    # List states
+    list_states(user, pwd, db, search)
