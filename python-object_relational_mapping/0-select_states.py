4d8c829dc6e8f1ec096f45cee4afa5275a45666b
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-10-03 14:58:18 +1000

    fix pycode

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,22 @@
 import MySQLdb
 import sys
 
-def list_states(username, password, database):
+
+def list_states(user, pwd, db):
     # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+    db = MySQLdb.connect(host="localhost", user=user, passwd=pwd, db=db, port=3306)
     cursor = db.cursor()
-    
+
     # Execute the query to select all states, ordered by states.id
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    
+
     # Fetch all the results
     states = cursor.fetchall()
-    
+
     # Display the results
     for state in states:
         print(state)
-    
+
     # Close the cursor and the database connection
     cursor.close()
     db.close()
@@ -26,11 +27,11 @@ if __name__ == "__main__":
     # Check if the correct number of arguments are provided
     if len(sys.argv) != 4:
         sys.exit(1)
-    
+
     # Get the arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
     # List states
-    list_states(username, password, database)
+    list_states(user, pwd, db)
