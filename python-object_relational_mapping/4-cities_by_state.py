dbc67991a1e991bb69af544fc5c6c14a210ba9d6
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-10-03 15:48:35 +1000

    fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,37 +1,47 @@
 #!/usr/bin/python3
-"""Module test"""
+"""Module to list all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
-
-def list_cities(user, pwd, db, search):
+def list_cities(user, pwd, db):
+    """List all cities from the specified database."""
     # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost",
-                         user=user, passwd=pwd, db=db, port=3306)
-    cursor = db.cursor()
-
-    # Execute the query
-    query = "SELECT * FROM cities"
-    cursor.execute(query, (search,))
-
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=pwd,
+        db=db,
+        port=3306
+    )
+    
+    cursor = db_connection.cursor()
+    
+    # Execute the query to select all cities ordered by cities.id
+    query = "SELECT cities.id, cities.name, states.name FROM cities " \
+            "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    
+    cursor.execute(query)
+    
     # Fetch all the results
     cities = cursor.fetchall()
-
+    
     # Display the results
     for city in cities:
         print(city)
 
     # Close the cursor and the database connection
     cursor.close()
-    db.close()
-
+    db_connection.close()
 
 if __name__ == "__main__":
+    # Check if the correct number of arguments are provided
+    if len(sys.argv) != 4:
+        sys.exit(1)
 
     # Get the arguments
     user = sys.argv[1]
     pwd = sys.argv[2]
     db = sys.argv[3]
 
-    # List states
+    # List cities
     list_cities(user, pwd, db)
