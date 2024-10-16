bd81f0a61577d380214eb39e3636ab24753d1f4c
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-10-17 10:07:07 +1100

    fix

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,32 +2,15 @@
 """
 A script that lists all State objects from the database
 hbtn_0e_6_usa.
-
-Usage: ./script_name.py <mysql username> <mysql password>
-<database name>
-
-This script:
-- Connects to a MySQL database using SQLAlchemy.
-- Retrieves all State objects sorted by id in ascending order.
-- Prints each State in the format: <id>: <name>.
 """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 def list_states(username, password, database):
     """
     Connects to a MySQL database and lists all State objects.
-    
-    Args:
-        username (str): The MySQL username.
-        password (str): The MySQL password.
-        database (str): The MySQL database name.
-    
-    Prints:
-        Each State object in the format: <id>: <name>.
     """
     engine = create_engine(
         'mysql+mysqldb://' + username + ':' + password +
@@ -56,8 +39,6 @@ def list_states(username, password, database):
 if __name__ == "__main__":
     # Ensure correct number of arguments
     if len(sys.argv) != 4:
-        print("Usage: ./script_name.py <mysql username> "
-              "<mysql password> <database name>")
         sys.exit(1)
 
     # Get MySQL credentials and database from command-line arguments
