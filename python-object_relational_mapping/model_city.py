787750ec23770f89300ac8ecbae3dc14c23dadb8
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-10-17 11:09:56 +1100

    fix

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -11,7 +11,7 @@ class City(Base):
     City class that represents the cities table.
     """
     __tablename__ = 'cities'
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
